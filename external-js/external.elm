9e208563fc708b32c92d2e09b33e116c72ad775b
Revath S Kumar <rsk@revathskumar.com>
2016-03-25 14:53:05 +0530

    Loading from external js

---
+++ external-js/external.elm
@@ -0,0 +1,24 @@
+{-
+type alias Model = Int
+
+type Action = Increment | Decrement
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    Increment -> model + 1
+    Decrement -> model - 1
+
+main =
+
+-}
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+main : Html
+main =
+  div [id "main"] [
+    div [class "welcome"] [text "Hello World!!"],
+    div [] [text "Another Message with external js"]
+  ]
