33468515fbaf9b1994944e9b0b248e274ebe3ab2
Revath S Kumar <rsk@revathskumar.com>
2016-03-27 12:45:42 +0530

    Move circle along with mouse

--- capture-mouse/mouse.elm
+++ capture-mouse/mouse.elm
@@ -3,67 +3,21 @@ import Graphics.Collage exposing (..)
 import Html exposing (..)
 import Color exposing (..)
 import Mouse
-{--
-view : (Int, Int) -> Element
-view pos =
-  let (x, y) = pos
-  in 
-    collage 200 200 
-    [ circle 20
-        |> filled(rgb 0 0 0)
-        |> move pos
-    ]
---}
+import Window
 
-view : (Int, Int) -> Html
-view pos = 
-  let (x, y) = pos
-  in 
+view : (Int, Int) -> (Int, Int) -> Html
+view (w, h) (x, y) =
+  div [] [
+    span [] [Html.text "Mouse Position :: "],
+    span [] [Html.text ("x = " ++ (toString x) ++ " y =  " ++ (toString y))],
     div [] [
-      span [] [Html.text "Mouse Position :: "],
-      span [] [Html.text ("x = " ++ (toString x) ++ " y =  " ++ (toString y))]
+      fromElement (collage w h 
+        [ circle 20
+            |> filled(rgb 0 0 0)
+            |> move ((toFloat x) - ((toFloat w) / 2), ((toFloat h) / 2) - (toFloat y))
+        ])
     ]
+  ]
 
-main = Signal.map view Mouse.position
-
-{--import Window
-import Mouse exposing (..)
-import Html exposing (..)
-
-type alias Position = (Int, Int)
-
-type Action = NoOp | UpdatePosition (Int, Int)
-
-initPosition : Position
-initPosition = (0, 0)
-
-update : Action -> Position -> Position
-update action pos =
-  case action of
-    NoOp ->
-      pos
-    UpdatePosition pos -> 
-      pos
-
-view : Signal.Address Action -> Position -> Html
-view action pos = 
-  let (x, y) = pos
-  in
-    div [] [
-      span [] [text "Mouse Position :: "],
-      span [] [text ("x = " ++ x ++ " y =  " ++ y)]
-    ]
-
-actions : Signal.Mailbox Action
-actions =
-  Signal.mailbox NoOp
-
-model : Signal Position
-model =
-  Signal.foldp update initPosition actions.signal
-
-main : Signal Html
-main =
-  Signal.map (view actions.address) model
---}
+main = Signal.map2 view Window.dimensions Mouse.position
 
